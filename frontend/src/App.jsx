73e138405664edb9f71eb93d5123b7fea15ba044
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2024-07-20 11:30:46 +0100

    Frontend: UI in progress

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SharedLayout from "./components/ShareLayout";
 import Mainfeed from "./components/Mainfeed";
@@ -18,20 +18,29 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<SharedLayout />}>
-            <Route index element={<AuthRoute element={<Mainfeed />} />} />
-            <Route path="/community" element={<AuthRoute element={<Community />} />} />
-            <Route path="/education" element={<AuthRoute element={<Education />} />} />
-            <Route path="/:postId/comments" element={<AuthRoute element={<Comment type="comment" />} />} />
-            <Route path="/post/:postId/subcomments" element={<AuthRoute element={<Comment type="subcomment" />} />} />
-            <Route path="/createpost" element={<AuthRoute element={<Createpost />} />} />
-            <Route path="/:postId/comment" element={<AuthRoute element={<Createcomment />} />} />
+            <Route indexelement={<Mainfeed />} />
+            <Route path="/community" element={<Community />} />
+            <Route path="/education" element={<Education />} />
+            <Route
+              path="/:postId/comments"
+              element={<Comment type="comment" />}
+            />
+            <Route
+              path="/post/:postId/subcomments"
+              element={<Comment type="subcomment" />}
+            />
+            <Route path="/createpost" element={<Createpost />} />
+            <Route path="/:postId/comment" element={<Createcomment />} />
           </Route>
 
           <Route path="/signup" element={<Signuppage />} />
-          <Route path="/register/verify/:userToken" element={<Emailconfirmation />} />
+          <Route
+            path="/register/verify/:userToken"
+            element={<Emailconfirmation />}
+          />
           <Route path="/login" element={<Loginpage />} />
           <Route path="/forgotpassword" element={<Forgotpasswordpage />} />
-          {/* <Route path="/wallet" element={<AuthRoute element={<Web3 />} />} /> */}
+          {/* <Route path="/wallet"element={<Web3 />} />} /> */}
         </Routes>
       </BrowserRouter>
     </div>
