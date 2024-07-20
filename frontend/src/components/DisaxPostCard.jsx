7804803cabe2071aec8fc2164422bff14c6c6721
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2024-07-20 16:27:28 +0100

    Frontend: Dashboard done, Finetuning and API integration left

---
+++ frontend/src/components/DisaxPostCard.jsx
@@ -0,0 +1,68 @@
+import Accountcard from "./Accountcard";
+import { AiFillHeart } from "react-icons/ai";
+import { IoChatboxEllipses } from "react-icons/io5";
+import { PiBookmarkFill } from "react-icons/pi";
+import { TbLineDashed } from "react-icons/tb";
+import { Link } from "react-router-dom";
+import { FaDonate } from "react-icons/fa";
+import { IoAlertCircle } from "react-icons/io5";
+import { RiAlarmWarningFill } from "react-icons/ri";
+
+export const DisaxPostCard = ({ post }) => {
+  const postUser = {
+    image: "",
+    username: post.username,
+    first_name: "WaverX",
+    userId: post.userId,
+  };
+  // console.log(post);
+
+  return (
+    <div className="disaster-post-card border-b-[1px] border-gray-700 py-4">
+      <Accountcard user={postUser} />
+      <div onClick={() => commentPage(post)}>
+        <p className="text-left text-sm px-3 my-3 ">{post.body}</p>
+        {post.image && (
+          <img className="w-[100%] px-3 " src={post.image} alt="" />
+        )}
+      </div>
+      <div className="flex flex-row justify-between px-3 mt-2 ">
+      <div className="flex flex-row items-center  ">
+          {post.isAlert ? (
+            <IoAlertCircle color="red" />
+          ) : (
+            <AiFillHeart size={18} />
+          )}
+        </div>
+        <div className="flex flex-row items-center">
+          <FaDonate size={18} />
+          <p className="text-xs ml-1 ">{post.comments_count}</p>
+        </div>
+        <Link onClick={() => setIsModalopen(true)}>
+          <div
+            className="flex flex-row items-center  "
+            // onClick={() => setIsModalopen(true)}
+          >
+            <IoChatboxEllipses size={18} />
+            <p className="text-xs ml-1 ">{post.comments_count}</p>
+          </div>
+        </Link>
+        <div
+          className="flex flex-row items-center"
+          // onClick={() => {
+          //   post.is_saved
+          //     ? unsaveMutation.mutate(post.id)
+          //     : saveMutation.mutate(post.id);
+          // }}
+        >
+          <PiBookmarkFill
+            size={18}
+            color={post.is_saved ? "rgb(0 128 128 / 1)" : ""}
+          />
+          <p className="text-xs ml-1 ">{post.savers_count}</p>
+        </div>
+       
+      </div>
+    </div>
+  );
+};
---
+++ frontend/src/components/Happeningnow.jsx
@@ -0,0 +1,26 @@
+import { DisaxPostCard } from './DisaxPostCard';
+import { useState, useEffect } from 'react'
+import { watchCollection } from "../services/firebase.service";
+
+const Happeningnow = () => {
+  const [posts, setPosts ] = useState([])
+  useEffect(()=>{
+    const path = "Posts"
+    watchCollection(path, (snapshot)=>{
+      const data = snapshot.docs.map(d=>({...d.data(), remoteId: d.id}))
+      const sorted = data.sort((a,b)=>b.postedAt - a.postedAt)
+      setPosts(data)
+    })
+  }, [])
+  return (
+    <div className='text-xl text-center   pt-1 md:pt-5 '>
+        <h2 className='pb-1 md:pb-3 text-lg md:text-xl border-gray-500 border-b-2 font-semibold '>Disasters Reports</h2>
+        {
+            posts.map(p=>{
+            return <DisaxPostCard key={p.remoteId} post={p} />
+          })
+          }    </div>
+  )
+}
+
+export default Happeningnow
