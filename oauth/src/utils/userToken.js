f7dc4649073df7981e6d7a7da8fadfa8f9d22bf9
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2024-07-21 02:59:24 +0100

    Deploying backend to heroku

--- oauth/src/utils/userToken.js
+++
@@ -1,7 +0,0 @@
-
-//Users details to encode with jwt
-const createTokenUser = (user) => {
-	return { name: user.name, userId: user._id, role: user.role };
-  };
-
-  module.exports = createTokenUser;
