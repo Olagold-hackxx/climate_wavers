a972a8fe64f3aa48bb78f809d1a603584e6463bc
Khemmie-Ray <atokemmy@gmail.com>
2024-08-23 02:40:18 +0100

    funds page change

---
+++ frontend/src/components/DonationTable.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
+
+const DonationTable = ({ currentData }) => {
+    
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow sx={{ borderBottom: '1px solid #dadada' }}>
+            <TableCell>
+              Details
+            </TableCell>
+            <TableCell>
+              Sent
+            </TableCell>
+            <TableCell>
+              Received
+            </TableCell>
+            <TableCell>
+              Donations
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+            <TableRow sx={{ borderBottom: '1px solid #dadada'}}>
+              <TableCell>
+                <h3 className='text-[#008080] font-[700]'>Volcano disaster donation</h3>
+                <p className='text-[#333333]/30 my-4'>Address: 0x45rgd.......3A</p>
+                <p className='text-[#333333]/30'>04 Apr, 2024m- 03:47 pm</p>
+              </TableCell>
+              <TableCell>------------</TableCell>
+              <TableCell>1000 <span className='text-[#008080] font-bold'>WAVER</span></TableCell>
+              <TableCell>
+                Donated
+              </TableCell>
+            </TableRow>
+            <TableRow sx={{ borderBottom: '1px solid #dadada '}}>
+              <TableCell>
+                <h3 className='text-[#008080] font-[700]'>Hurricane Management</h3>
+                <p className='text-[#333333]/50 my-4'>Address: 0x45rgd.......3A</p>
+                <p className='text-[#333333]/50'>04 Apr, 2024m- 03:47 pm</p>
+              </TableCell>
+              <TableCell>------------</TableCell>
+              <TableCell>20,000 <span className='text-[#008080] font-bold'>WAVER</span></TableCell>
+              <TableCell>
+                Donated
+              </TableCell>
+            </TableRow>
+            <TableRow sx={{ borderBottom: '1px solid #dadada '}}>
+              <TableCell>
+                <h3 className='text-[#008080] font-[700]'>Fire Disasters</h3>
+                <p className='text-[#333333]/30 my-4'>Address: 0x45rgd.......3A</p>
+                <p className='text-[#333333]/30'>04 Apr, 2024m- 03:47 pm</p>
+              </TableCell>
+              <TableCell>------------</TableCell>
+              <TableCell>50,000 <span className='text-[#008080] font-bold'>WAVER</span></TableCell>
+              <TableCell>
+                Donated
+              </TableCell>
+            </TableRow>
+            <TableRow sx={{ borderBottom: '1px solid #dadada '}}>
+              <TableCell>
+                <h3 className='text-[#008080] font-[700]'>South Africa wild fire</h3>
+                <p className='text-[#333333]/30 my-4'>Address: 0x45rgd.......3A</p>
+                <p className='text-[#333333]/30'>04 Apr, 2024m- 03:47 pm</p>
+              </TableCell>
+              <TableCell>------------</TableCell>
+              <TableCell>5,000 <span className='text-[#008080] font-bold'>WAVER</span></TableCell>
+              <TableCell>
+                Donated
+              </TableCell>
+            </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default DonationTable;
--- frontend/src/components/Funds.jsx
+++ frontend/src/components/Funds.jsx
@@ -1,92 +1,80 @@
-import Wallet from "./Wallet";
-import { useWeb3ModalAccount } from "@web3modal/ethers/react";
+import React, { useState, useEffect } from 'react';
+import { ethers } from 'ethers';
+import { getProvider } from '../constants/providers';
+import { useWeb3ModalProvider, useWeb3ModalAccount } from "@web3modal/ethers/react";
+import DonationTable from './DonationTable';
+import { IoEye, IoCopyOutline } from "react-icons/io5";
+import { IoMdEyeOff } from "react-icons/io";
+import Deposit from './Deposit';
 
 const Funds = () => {
-  const dataInfo = [
-    {
-      id: 1,
-      image:
-        "https://img.freepik.com/free-photo/happy-woman-with-short-hair_1308-171290.jpg?size=626&ext=jpg&ga=GA1.1.663094344.1720174275&semt=sph",
-      name: "Haitians",
-    },
-    {
-      id: 2,
-      image:
-        "https://img.freepik.com/free-photo/3d-illustration-teenager-with-funny-face-glasses_1142-50955.jpg?size=626&ext=jpg&ga=GA1.2.663094344.1720174275&semt=sph",
-      name: "Evans",
-    },
-    {
-      id: 3,
-      image:
-        "https://img.freepik.com/free-vector/animated-female-face-with-brown-hair_1308-171070.jpg?size=626&ext=jpg&ga=GA1.2.663094344.1720174275&semt=sph",
-      name: "Gabriela",
-    },
-    {
-      id: 4,
-      image:
-        "https://img.freepik.com/free-vector/hand-drawn-people-with-dreadlocks-illustration_23-2149752847.jpg?size=626&ext=jpg&ga=GA1.2.663094344.1720174275&semt=sph",
-      name: "SoS Global",
-    },
-    {
-      id: 5,
-      image:
-        "https://img.freepik.com/free-psd/3d-rendering-avatar_23-2150833546.jpg?size=626&ext=jpg&ga=GA1.2.663094344.1720174275&semt=sph",
-      name: "Childfare",
-    },
-  ];
-  const { isConnected } = useWeb3ModalAccount();
+  const { walletProvider } = useWeb3ModalProvider();
+  const { address } = useWeb3ModalAccount();
+  const [balance, setBalance] = useState(0);
+  const [open, setOpen] = useState(true);
+  const [copied, setCopied] = useState(false);
+
+  useEffect(() => {
+    const fetchWalletDetails = async () => {
+      try {
+        const readWriteProvider = getProvider(walletProvider);
+        const walletBalance = await readWriteProvider.getBalance(address);
+        const formattedBalance = ethers.formatEther(walletBalance);
+        const roundedBalance = parseFloat(formattedBalance).toFixed(2);
+        setBalance(roundedBalance);
+      } catch (error) {
+        console.error('Error fetching wallet details:', error);
+      }
+    };
+
+    if (walletProvider) {
+      fetchWalletDetails();
+    }
+  }, [walletProvider]);
+
+  const shortenAddress = `${address?.slice(0, 7)}.........${address?.slice(-2)}`;
+
+  const toggleBalanceVisibility = () => {
+    setOpen(!open);
+  };
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(address).then(() => {
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000);
+    });
+  };
 
   return (
-    <main className="p-8">
-      {!isConnected && <Wallet />}
-
-      <h2 className="text-[25px] font-[700] my-4">Donation History</h2>
-      <section className="bg-[#008080] rounded-lg py-2 px-6 mb-8">
-        <h3 className="text-[21px] my-4 font-[700]">Beneficiaries</h3>
-        <div className="flex">
-          {dataInfo.map((info) => (
-            <div key={info.id} className="px-4">
-              <img
-                src={info.image}
-                alt=""
-                className="w-[60px] h-[60px] rounded-full"
-              />
-              <p className="text-center mt-2 text-[15px]">{info.name}</p>
-            </div>
-          ))}
+    <main className='my-8'>
+      <h2 className='text-[20px] lg:text-[28px] md:text-[28px] my-4 font-[700]'>Your Wallet</h2>
+      <div className='flex items-center'>
+      <p className='text-[18px] lg:text-[24px] md:text-[24px] truncate'>{shortenAddress}</p>
+      <IoCopyOutline 
+          className='text-2xl'
+          onClick={copyToClipboard} 
+          style={{ cursor: 'pointer', marginLeft: '8px' }} 
+          title={copied ? "Copied!" : "Copy Address"} 
+        />
+        {copied && <span className="text-green-500 ml-2 text-sm">Copied!</span>}
         </div>
+      <div className='flex items-center my-6'>
+        <h2 className='lg:text-[56px] md:text-[56px] text-[36px]'>{open ? `${balance} ETH` : "***********"} </h2>
+        {open ? (
+          <IoMdEyeOff onClick={toggleBalanceVisibility} className='text-4xl mx-8' style={{ cursor: 'pointer' }} />
+        ) : (
+          <IoEye onClick={toggleBalanceVisibility} className='text-4xl mx-8' style={{ cursor: 'pointer' }} />
+        )}
+        <p className='bg-[#008080]/25 rounded-lg text-[#008080] py-2 px-4'>$30.00</p>
+      </div>
+      <section className='flex items-center flex-col lg:flex-row md:flex-row'>
+       <Deposit address={address} />
+        <button className='bg-[#008080] py-2 px-12 lg:mr-6 md:mr-6 mb-4 rounded-full text-white text-[18px] lg:text-[24px] md:text-[24px]'>Withdraw</button>
+        <button className='bg-[#008080] py-2 px-12 lg:mr-6 md:mr-6 mb-4 rounded-full text-white text-[18px] lg:text-[24px] md:text-[24px]'>+ Buy</button>
       </section>
-      <section className="bg-[#008080] rounded-lg py-2 px-6">
-        <h3 className="text-[21px] my-4 font-[700]">Recent Transactions</h3>
-        <table className="w-[100%]">
-          <thead>
-            <tr>
-              <td className="text-white py-6">Photos</td>
-              <td className="text-white">Username</td>
-              <td className="text-white">Date</td>
-              <td className="text-white">Status</td>
-              <td className="text-white">Amount</td>
-              <td className="text-white">Action</td>
-            </tr>
-          </thead>
-
-          <tbody>
-            <tr>
-              <td className="text-white">
-                <img
-                  src="https://img.freepik.com/free-photo/portrait-beautiful-girl-yellow-coat-with-backpack_1142-55171.jpg?t=st=1720751388~exp=1720754988~hmac=0b252e2053a1ec9bb9c49320b3d4944483e2542fe676d0b6430fdb983298f61b&w=1380"
-                  alt=""
-                  className="w-[40px] h-[40px] rounded-lg"
-                />
-              </td>
-              <td className="text-white">Desmond</td>
-              <td className="text-white">2024</td>
-              <td className="text-white">Pending</td>
-              <td className="text-white">20DSX</td>
-              <td className="text-white">Details</td>
-            </tr>
-          </tbody>
-        </table>
+      <section>
+        <h2 className='text-[18px] lg:text-[24px] md:text-[24px] my-4'>All Transaction details</h2>
+        <DonationTable />
       </section>
     </main>
   );
