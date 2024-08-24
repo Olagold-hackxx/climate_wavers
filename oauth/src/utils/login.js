50cdd2635827c8056e5fa6449a3a16af8a488f1c
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2024-08-24 06:33:39 +0100

    Deploy models

--- oauth/src/utils/login.js
+++ oauth/src/utils/login.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 const localLogin = async (data) => {
     try {
         console.log(data)
-        const response = await axios.post(`${process.env.BACKEND}/api/user/login/`, data);
+        const response = await axios.post(`${process.env.BACKEND}/api/v1/auth/login/`, data);
         return response.data;
     } catch (error) {
         console.log(`Failed to login user: ${error}`)
--- oauth/src/utils/register.js
+++ oauth/src/utils/register.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const localRegister = async (data) => {
     try {
         console.log(data)
-        const response = await axios.post(`${process.env.BACKEND}/api/user/register/`, data);
+        const response = await axios.post(`${process.env.BACKEND}/api/v1/auth/register/`, data);
         return response.data;
     } catch (error) {
         return new Error(`Failed to register user: ${error}`);
