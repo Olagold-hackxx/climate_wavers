bf36ba73bef042216919acfa4f19fa4b0c711d0c
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2024-08-25 05:33:10 +0100

    Set samesite attribute for cookie to allow cross-site cookie for open auth

--- oauth/src/utils/jwt.js
+++ oauth/src/utils/jwt.js
@@ -28,6 +28,7 @@ const attachCookiesToResponse = ({ res, user }) => {
 
   res.cookie("user", JSON.stringify(user), {
     httpOnly: false,
+    sameSite: 'none',
     secure: process.env.NODE_ENV === "production",
     signed: false,
     expires: new Date(Date.now() + oneDay),
@@ -36,6 +37,7 @@ const attachCookiesToResponse = ({ res, user }) => {
 
   res.cookie("accessToken", user.access_token, {
     httpOnly: false,
+    sameSite: 'none',
     secure: process.env.NODE_ENV === "production",
     signed: false,
     expires: new Date(Date.now() + longerExp),
