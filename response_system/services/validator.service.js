62d672905504c945fac201c2c5475b4c4e70a8ac
Horlarhyinka <danijufarouq2003@gmail.com>
2024-08-28 19:14:34 +0100

    response_system: created create and list controller for disasters

--- response_system/services/validator.service.js
+++ response_system/services/validator.service.js
@@ -56,6 +56,25 @@ class Validator{
             data: Joi.object().unknown(true)
         }).validate(obj)
     }
+
+    validateCreateDisasterPayload(obj){
+        return Joi.object({
+            startDate: Joi.number().default(Date.now()),
+            endDate: Joi.number(),
+            totalDeath: Joi.number().default(0),
+            injured: Joi.number().default(0),
+            homeless: Joi.number().default(0),
+            magnitude: Joi.number(),
+            magnitudeScale: Joi.string(),
+            coordinate: Joi.object({
+                lng: Joi.number().required(),
+                lat: Joi.number().required()
+            }),
+            disasterType: Joi.string().required(),
+            country: Joi.string().required(),
+            region: Joi.string().required()
+        }).validate(obj)
+    }
 }
 
 const validator = new Validator()
